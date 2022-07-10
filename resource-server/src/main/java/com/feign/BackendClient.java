e00c143436b59bd5a12029f83e03bbfc89f7bbfd
unknown <jupiter85838@yahoo.com>
2022-07-10 01:04:47 -0400

    add feign config/logging

--- resource-server/src/main/java/com/feign/BackendClient.java
+++ resource-server/src/main/java/com/feign/BackendClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "feign-resource-service", url = "${sample.client.url}")
+@FeignClient(name = "feign-resource-service", url = "${sample.client.url}", configuration = FeignConfig.class)
 
 public interface BackendClient {
 
@@ -18,7 +18,6 @@ public interface BackendClient {
 	@RequestMapping(method = RequestMethod.GET, value = "/country/{name}")
 	Country geCountry(@PathVariable String name);
 
-	
 	@RequestMapping(method = RequestMethod.POST, value = "/country")
 	List<Country> create(@RequestBody Country newCountry);
 
